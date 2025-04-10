8e162387e08dc8591bc7fae1f56521bf736f957c
Bond, James Bond <kengerlinskyemil@yandex.ru>
2025-04-10 18:26:37 +0300

    first commit

--- precode.go
+++ precode.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -47,9 +49,100 @@ func main() {
 
 	// здесь регистрируйте ваши обработчики
 	// ...
-
+	r.Get("/tasks", ListTasksHandler)
+	r.Post("/tasks", AddTaskHandler)
+	r.Get("/task/{id}", GetTaskByIdHandler)
+	r.Delete("/task/{id}", DeleteTaskByIdHandler)
 	if err := http.ListenAndServe(":8080", r); err != nil {
 		fmt.Printf("Ошибка при запуске сервера: %s", err.Error())
 		return
 	}
+}
+
+func ListTasksHandler(w http.ResponseWriter, req *http.Request) {
+	tasksMarshaled, err := json.Marshal(tasks)
+	if err != nil {
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		log.Println(err)
+		return
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	_, err = w.Write(tasksMarshaled)
+	if err != nil {
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		log.Println(err)
+		return
+	}
+}
+
+func AddTaskHandler(w http.ResponseWriter, req *http.Request) {
+	task := Task{}
+	err := json.NewDecoder(req.Body).Decode(&task)
+	if err != nil {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		log.Println(err)
+		return
+	}
+
+	_, ok := tasks[task.ID]
+	if ok {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		log.Println("bad id")
+		return
+	}
+
+	tasks[task.ID] = task
+
+	w.WriteHeader(http.StatusCreated)
+}
+
+func GetTaskByIdHandler(w http.ResponseWriter, req *http.Request) {
+	id := chi.URLParam(req, "id")
+	if id == "" {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		log.Println("no id in url")
+		return
+	}
+
+	task, ok := tasks[id]
+	if !ok {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		log.Println("no task with such id")
+		return
+	}
+
+	taskMarshaled, err := json.Marshal(task)
+	if err != nil {
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		log.Println(err)
+		return
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+
+	_, err = w.Write(taskMarshaled)
+	if err != nil {
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		log.Println(err)
+		return
+	}
+
+}
+func DeleteTaskByIdHandler(w http.ResponseWriter, req *http.Request) {
+	id := chi.URLParam(req, "id")
+	if id == "" {
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		log.Println("no id in url")
+		return
+	}
+
+	_, ok := tasks[id]
+	if !ok {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		log.Println("no task with such id")
+		return
+	}
+	delete(tasks, id)
+	w.WriteHeader(http.StatusOK)
 }
